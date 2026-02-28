d36c05d5eba6d161696ab74b213e6e611bbcde43
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-02-28 09:01:34 +0530

    V pattern done using nested loops

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ let prompt = require('prompt-sync')();
 let n = Number(prompt('Enter a Number'));
 
 for (let i = 1; i<=n; i++){
-    for(let j=1; j<=n;j++){
-        if(i==j || i+j==n+1){
+    for(let j=1; j<=n*2-1;j++){
+        
+        if(i==j || i+j==n*2){
 
             process.stdout.write('* ')
         }else{
