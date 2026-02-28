d89c9bd966dc9eb340d3e72e32487fd2d3861eaa
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-02-28 08:49:18 +0530

    X pattern programing

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-for(let i = 5 ; i>=1; i--){
-    for(let j=5; j>=5-1 ; j-- ){
-       process.stdout.write(' ') 
-    }
-    for(let j=5; j>=i;j--){
-        process.stdout.write('* ')
+// for(let i = 5 ; i>=1; i--){
+
+//    for(let j=1;j<=i;j++){
+//                 process.stdout.write('  ');
+//             }
+  
+//     for(let j=5; j>=i;j--){
+//         process.stdout.write('* ')
+//     }
+//     console.log()
+// }
+
+let prompt = require('prompt-sync')();
+let n = Number(prompt('Enter a Number'));
+
+for (let i = 1; i<=n; i++){
+    for(let j=1; j<=n;j++){
+        if(i==j || i+j==n+1){
+
+            process.stdout.write('* ')
+        }else{
+            process.stdout.write(' ')
+        }
     }
     console.log()
 }
