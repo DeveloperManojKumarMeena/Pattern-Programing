71ea7476b6dbf12d07cb545aa67f2764e480cbac
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-02-28 09:20:16 +0530

    butterfly pattern

--- app.js
+++ app.js
@@ -13,14 +13,14 @@
 let prompt = require('prompt-sync')();
 let n = Number(prompt('Enter a Number'));
 
-for (let i = 1; i<=n; i++){
+for (let i = 1; i<=n*2-1; i++){
     for(let j=1; j<=n*2-1;j++){
         
-        if(i==j || i+j==n*2){
+        if(i==j || i+j==n*2|| j==1 || j==9){
 
             process.stdout.write('* ')
         }else{
-            process.stdout.write(' ')
+            process.stdout.write('  ')
         }
     }
     console.log()
